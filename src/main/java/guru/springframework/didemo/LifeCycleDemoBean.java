557bf266d263973dd95213cea5a5d7b42859d377
todd <toddtran9@gmail.com>
2018-06-11 18:37:53 -0700

    Adding in a few comments to document the order each of these methods are run in.

--- src/main/java/guru/springframework/didemo/LifeCycleDemoBean.java
+++ src/main/java/guru/springframework/didemo/LifeCycleDemoBean.java
@@ -18,48 +18,57 @@ public class LifeCycleDemoBean implements InitializingBean, DisposableBean, Bean
         System.out.println("## I'm in the LifeCycleBean Constructor");
     }
 
+    // 9
     @Override
     public void destroy() throws Exception {
         System.out.println("## The Lifecycle bean has been terminated");
 
     }
 
+    // 6
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("## The LifeCycleBean has its properties set!");
 
     }
 
+    // 2
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         System.out.println("## Bean Factory has been set");
     }
 
+    // 1
     @Override
     public void setBeanName(String name) {
         System.out.println("## My Bean Name is: " + name);
 
     }
 
+    // 3
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("## Application context has been set");
     }
 
+    // 5
     @PostConstruct
     public void postConstruct(){
         System.out.println("## The Post Construct annotated method has been called");
     }
 
+    // 8
     @PreDestroy
     public void preDestroy() {
         System.out.println("## The Predestroy annotated method has been called");
     }
 
+    // 4
     public void beforeInit(){
         System.out.println("## - Before Init - Called by Bean Post Processor");
     }
 
+    // 7
     public void afterInit(){
         System.out.println("## - After init called by Bean Post Processor");
     }
